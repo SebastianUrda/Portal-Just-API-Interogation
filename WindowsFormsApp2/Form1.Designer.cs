ff3ecf8cd4d47a4842dcba6996101b61ac88e575
Sebastian <sebastian.george.urda@gmail.com>
2020-09-23 13:49:22 +0300

    Initial commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.genereaza = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // genereaza
+            // 
+            this.genereaza.Location = new System.Drawing.Point(608, 83);
+            this.genereaza.Name = "genereaza";
+            this.genereaza.Size = new System.Drawing.Size(75, 23);
+            this.genereaza.TabIndex = 0;
+            this.genereaza.Text = "Genereaza";
+            this.genereaza.UseVisualStyleBackColor = true;
+            this.genereaza.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(153, 83);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(380, 82);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "De la data:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pana la data:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(26, 139);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(657, 139);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Materie";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 295);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.genereaza);
+            this.Name = "Form1";
+            this.Text = "Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button genereaza;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        PortalWS.Query service= new PortalWS.Query();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DataSource = Enum.GetValues(typeof(PortalWS.CategorieCaz));
+        }
+
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            List<PortalWS.Dosar> rezultateFinale = new List<PortalWS.Dosar>();
+            if (dateTimePicker1.Value > dateTimePicker2.Value)
+            {
+                MessageBox.Show("A doua data trebuie sa fie mai mica decta prima!");
+            }
+            else
+            {
+                string fileName = "Dosare " + comboBox1.Text +" "+ dateTimePicker1.Value.ToShortDateString().Replace("/", ".") + "-" + dateTimePicker2.Value.ToShortDateString().Replace("/", ".") + ".csv";
+                richTextBox1.AppendText("A inceput cautarea\n");
+                if (!CreateHeader(fileName))
+                {
+                    MessageBox.Show("A aparut o eroare in creerea fisierului!");
+                }
+                Task.Factory.StartNew(() => appendDosarToFile(watch, rezultateFinale, fileName));
+            }
+
+        }
+
+        private void appendDosarToFile(System.Diagnostics.Stopwatch watch, List<PortalWS.Dosar> rezultateFinale, string fileName)
+        {
+            int counter = 0;
+            foreach (PortalWS.Institutie institutie in (PortalWS.Institutie[])Enum.GetValues(typeof(PortalWS.Institutie)))
+            {
+                PortalWS.Dosar[] ret = new PortalWS.Dosar[0];
+                try
+                {
+                    ret = service.CautareDosare(null, null, null, institutie, Convert.ToDateTime(dateTimePicker1.Value.ToShortDateString()), Convert.ToDateTime(dateTimePicker2.Value.ToShortDateString()));
+                }
+                catch (Exception exception)
+                {
+                    String errorMessage = "Eroare in cautarea institutiei: " + institutie.ToString(); 
+                    richTextBox1.Invoke(new Action<string>(AppendText), errorMessage + "\n");
+                }
+
+                if (ret == null)
+                {
+
+                }
+                else
+                {
+                    string selectedcategroieCaz = "Toate";
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        selectedcategroieCaz = comboBox1.Text;
+                    }));
+                    foreach (PortalWS.Dosar dosar in ret)
+                    {
+                        if (selectedcategroieCaz.Equals("Toate"))
+                        {
+                            rezultateFinale.Add(dosar);
+                            if (!AddRecord(dosar, fileName))
+                            {
+                                richTextBox1.Invoke(new Action<string>(AppendText), "Eroare in adaugarea dosarului " + dosar.numar);
+                            }
+                        }
+                        else
+                        {
+                            if (selectedcategroieCaz.Equals(dosar.categorieCaz.ToString()))
+                            {
+                                rezultateFinale.Add(dosar);
+                                if (!AddRecord(dosar, fileName))
+                                {
+                                    richTextBox1.Invoke(new Action<string>(AppendText), "Eroare in adaugarea dosarului " + dosar.numar);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (counter != rezultateFinale.Count)
+                {
+                    int rez = rezultateFinale.Count - counter;
+                    richTextBox1.Invoke(new Action<string>(AppendText), "S-au gasit in: " + institutie + " " + rez + " rezultate\n");
+                    counter = rezultateFinale.Count;
+                }
+
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            MessageBox.Show("Fisierul a fost create cu success!\n Contine " + rezultateFinale.Count + " rezultate\nA durat " + elapsedMs / 1000 + " secunde.");
+        }
+
+        private void AppendText(string obj)
+        {
+            richTextBox1.AppendText(obj);
+        }
+
+        private Boolean CreateHeader(string filePath)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filePath, true))
+            {
+                try
+                {
+                    string header = "Instanta,";
+                    header += "Numar,";
+                    header += "Date,";
+                    header += "Materie,";
+                    header += "Obiect,";
+                    header += "Debitor,";
+                    header += "Creditor,";
+                    header += "Alte parti,";
+                    file.WriteLine(header);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.Write(ex);
+                    return false;
+                }
+               
+            }
+            return true;
+        }
+
+        private Boolean AddRecord(PortalWS.Dosar dosar, string filePath)
+        {
+            try
+            {
+                string toWrite = dosar.institutie.ToString()+",";
+                if (dosar.numar != null)
+                {
+                    toWrite += dosar.numar + ",";
+                }
+                else
+                {
+                    toWrite += ",";
+                }
+                if (dosar.data != null)
+                {
+                    toWrite += dosar.data.ToShortDateString() + ",";      
+                }
+                else
+                {
+                    toWrite += ",";
+                }
+                if (dosar.categorieCazNume != null)
+                {
+                    toWrite += dosar.categorieCazNume.Replace(",", " ") + ",";
+                }
+                else
+                {
+                    toWrite += ",";
+                }
+                if (dosar.obiect != null)
+                {
+                    toWrite += dosar.obiect.Replace(",", " ") + ",";
+                }
+                else
+                {
+                    toWrite += ",";
+                }
+                if (dosar.parti!=null)
+                {
+                    List<PortalWS.DosarParte> debitori = new List<PortalWS.DosarParte>();
+                    List<PortalWS.DosarParte> creditori = new List<PortalWS.DosarParte>();
+                    List<PortalWS.DosarParte> alteParti = new List<PortalWS.DosarParte>();
+                    foreach (PortalWS.DosarParte parte in dosar.parti)
+                    {
+                        
+                        if ("Debitor".Equals(parte.calitateParte))
+                        {
+                            debitori.Add(parte);
+                        }
+                        if ("Creditor".Equals(parte.calitateParte))
+                        {
+                            creditori.Add(parte);
+                        }
+                        else
+                        {
+                            alteParti.Add(parte);
+                        }
+                        
+                    }
+                    string toWriteDebitori = "";
+                    foreach(PortalWS.DosarParte parte in debitori)
+                    {
+                        toWriteDebitori += parte.nume.Replace(",", " ") + ";";
+                    }
+                    string toWriteCreditori = "";
+                    foreach (PortalWS.DosarParte parte in creditori)
+                    {
+                        toWriteCreditori += parte.nume.Replace(",", " ") + ";";
+                    }
+                    string toWriteAletParti = "";
+                    foreach (PortalWS.DosarParte parte in alteParti)
+                    {
+                        toWriteAletParti += parte.nume.Replace(",", " ") + ";";
+                    }
+                    toWrite += toWriteDebitori + "," + toWriteCreditori + "," + toWriteAletParti;
+                }
+                using (System.IO.StreamWriter file=new System.IO.StreamWriter(@filePath, true,Encoding.Default))
+                {
+                    file.WriteLine(toWrite);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Error.Write(ex);
+                return false;
+            }
+            return true;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+    }
+}
